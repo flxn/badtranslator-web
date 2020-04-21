522b6cdcf30cc54b2bcdbd3e54b6a1fd73d78b3c
a73841959 <1310010428@qq.com>
2020-04-21 19:05:06 +0800

    Added Chinese translation

---
+++ src/translations/cn.js
@@ -0,0 +1,50 @@
+export default {
+    //
+    //  Local name of the language
+    //
+    languageLocalName: '简体中文',
+  
+    //
+    // General
+    //
+    changeLanguage: '切换语言',
+    supportMe: '支持 BadTranslator',
+    shareButtonTitle: '分享本页面',
+    subtitle: '一个<em>Chinese Whispers</em>(传话游戏)这样的游戏,但它使用网上的翻译服务. <br />通过多种语言翻译你的文本并返回你初始的语言',
+    selectLanguage: '选择你要输入的语言',
+    translationRoundsHelp: '选择你希望你的语言转换多少次.',
+    translationRounds: '转换 {n} 次',
+    useExample: '使用一个例子',
+    textInputPlaceholder: '输入任何你想翻译的文本',
+    translateButton: '翻译！',
+    shareTranslation: '分享这次翻译',
+    shareTranslationTitle: '这是你通过随机语言翻译时会发生的情况',
+    tryAgain: '使用其它语言再试一次!',
+    tryAgainDescription: '太离谱了? 尝试减少翻译的次数.<br />不够有趣？ 尝试增加翻译的次数.',
+    howItWorksTitle: '它是如何工作的?',
+    howItWorksParagraph1: '这个网站基于\
+      <em>telephone</em> 这个游戏, 也被称为\
+      <em>chinese whispers</em>(传话游戏), 只不过是通过网上的翻译服务而不是真人.\
+      你的文本将会被多种随机语言翻译并返回它初始的语言.',
+    howItWorksParagraph2: '这是\
+      <a href="https://en.wikipedia.org/wiki/Chinese_whispers" rel="noopener" target="_blank">\
+        维基百科</a>\
+      关于"Chinese Whispers"的第一段，它被六种不同的语言翻译并返回以下内容:',
+    howItWorksQuote: "文字游戏老的游戏-这不是一个孩子，说到团队，在耳语，或者身体的语言，例如在第一页，在项规则，然后，如果游戏检测到的结束，原来一样，翻译已成为。 注意到在比赛期间，如果两个人可以不通过第三方，这是什么感兴趣我。 这个游戏的目标的原因是，转移到一个谣言，或是一个传奇，讲解什么是外部的，从而血液循环或是否作添油加醋。",
+    supportModalTitle: '你好啊',
+    supportModalText: "很抱歉打扰了你,但是我发现你使用BadTranslator已经{translationCount}次了 . 希望你喜欢我们的应用并开怀大笑 😊\
+      <br />这个网站永远免费但可惜的是我正在使用的网络翻译服务并不是免费的.\
+      <br />\
+      <br />如果你喜欢这个应用请考虑捐赠，这样我可以保持这个服务持续运行.",
+    supportModalThanks: "如果你不能捐赠那也没有关系但请考虑告诉你的朋友关于BadTranslator.\
+      <br />非常感谢你的支持，玩得开心!",
+    donateOnX: "选择{x}捐赠",
+    alreadyDonated: '我已经捐赠啦！',
+    thinkingAboutDonating: "我会认真考虑捐赠的",
+    wontDonate: '一边儿去吧傻子！',
+    
+    shareTitle: '享受BadTranslator',
+    shareText: '在BadTranslator.net 你可以玩 "Telephone Game" 和网络翻译器. 用随机的语言翻译你的文本，看看会发生什么.',
+    
+  };
+  
